a82fe41fc8c6c01dbfd81b8be827cdea73d810c2
Paul Mitsch <paul.herzog@urlaubsguru.de>
2026-03-30 20:55:09 +0200

    Remove not needed imports

--- src/main/kotlin/repository/UserRepository.kt
+++ src/main/kotlin/repository/UserRepository.kt
@@ -1,11 +1,6 @@
 package app.sotchi.repository
 
 import app.sotchi.domain.user.UserEntity
-import app.sotchi.dto.user.UserCreateDTO
-import app.sotchi.dto.user.UserLoginDTO
-import app.sotchi.dto.user.UserProfileDTO
-import app.sotchi.dto.user.UserReadDTO
-import app.sotchi.dto.user.UserUpdateDTO
 
 interface UserRepository {
     fun findById(id: Int): UserEntity?
