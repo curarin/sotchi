016da11fdcc4b138a10761982edbb5d3cedb2211
Paul Mitsch <paul.herzog@urlaubsguru.de>
2026-03-30 21:20:03 +0200

    Bump exposed and h2 version, adapt config

--- src/main/kotlin/plugins/Databases.kt
+++ src/main/kotlin/plugins/Databases.kt
@@ -1,9 +1,9 @@
 package app.sotchi.plugins
-import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.v1.jdbc.Database
 
 fun configureDatabases() {
     val database = Database.connect(
-        url = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1",
+        url = "jdbc:h2:mem:test",
         user = "root",
         driver = "org.h2.Driver",
         password = "",
