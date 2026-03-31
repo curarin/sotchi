a67df8f6a5f9cdc23229d1e058032cf4a4f2da8c
Paul Mitsch <paul.herzog@urlaubsguru.de>
2026-03-31 22:58:52 +0200

    Remove not needed imports

--- src/main/kotlin/controller/routes/UserRoutes.kt
+++ src/main/kotlin/controller/routes/UserRoutes.kt
@@ -14,7 +14,6 @@ import io.ktor.server.request.*
 import io.ktor.server.resources.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.Route
-import kotlin.text.toIntOrNull
 
 
 fun Route.userRoutesV1(userService: UserService) {
@@ -23,7 +22,6 @@ fun Route.userRoutesV1(userService: UserService) {
      */
     get<UserAuth.Read> {
         val user = call.receive<UserReadDTO>()
-        //val userId = call.request.headers["user-id"]?.toIntOrNull() ?: return@get call.respond(HttpStatusCode.Unauthorized)
         try {
             val existingUser = userService.read(user)
             call.respond(HttpStatusCode.OK, existingUser)
@@ -81,7 +79,6 @@ fun Route.userRoutesV1(userService: UserService) {
     delete<UserAuth.Delete> {
         val user = call.receive<UserReadDTO>()
         val userId = user.id
-        //val userId = call.request.headers["user-id"]?.toIntOrNull() ?: return@delete call.respond(HttpStatusCode.Unauthorized)
         try {
             val userIsDeleted = userService.delete(userId)
             call.respond(HttpStatusCode.OK, "User successfully deleted: $userIsDeleted")
