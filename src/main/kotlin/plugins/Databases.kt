b72f7d3436a5286cc533b26aa6b0347c60e4fabd
Paul Mitsch <paul.herzog@urlaubsguru.de>
2026-03-31 06:28:11 +0200

    Add database Config for defaultMaxAttempts, Add Config so H2 persists after closing, Add SchemaUtils Create for UserTable

--- src/main/kotlin/plugins/Databases.kt
+++ src/main/kotlin/plugins/Databases.kt
@@ -1,11 +1,24 @@
 package app.sotchi.plugins
+import app.sotchi.repository.UserTable
+import org.jetbrains.exposed.v1.core.DatabaseConfig
 import org.jetbrains.exposed.v1.jdbc.Database
+import org.jetbrains.exposed.v1.jdbc.SchemaUtils
+import org.jetbrains.exposed.v1.jdbc.transactions.JdbcTransactionManager
+import org.jetbrains.exposed.v1.jdbc.transactions.transaction
+import java.sql.Connection
 
 fun configureDatabases() {
     val database = Database.connect(
-        url = "jdbc:h2:mem:test",
+        url = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1",
         user = "root",
         driver = "org.h2.Driver",
         password = "",
+        //transactionIsolation = Connection.TRANSACTION_READ_COMMITTED
+        databaseConfig = DatabaseConfig {
+            defaultMaxAttempts = 3
+        }
     )
+    transaction {
+        SchemaUtils.create(UserTable)
+    }
 }
