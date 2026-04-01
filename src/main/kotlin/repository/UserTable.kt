6126e2c161d1ba3f5519286300afd72076a4f1c3
Paul Mitsch <paul.herzog@urlaubsguru.de>
2026-04-01 13:35:14 +0200

    Add role to DB schema

--- src/main/kotlin/repository/UserTable.kt
+++ src/main/kotlin/repository/UserTable.kt
@@ -1,4 +1,5 @@
 package app.sotchi.repository
+import app.sotchi.domain.generic.UserRole
 import org.jetbrains.exposed.v1.core.dao.id.IntIdTable
 import org.jetbrains.exposed.v1.datetime.*
 
@@ -10,4 +11,5 @@ object UserTable : IntIdTable() {
     val password = varchar("password", 256)
     val createdAtDt = timestamp("created_at_dt")
     val lastModifiedDt = timestamp("last_modified_dt")
+    val role = enumerationByName<UserRole>("role", 20)
 }
