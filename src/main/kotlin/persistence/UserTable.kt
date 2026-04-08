c7ae1846a08059c2f70bbae6c50b9067c451e58b
Paul Mitsch <paul.herzog@urlaubsguru.de>
2026-04-08 14:27:46 +0200

    chore<#2>: Remove constant for varchar length and use custom length for each attribute, add custom name to table so its more expressive

--- src/main/kotlin/persistence/UserTable.kt
+++ src/main/kotlin/persistence/UserTable.kt
@@ -4,11 +4,10 @@ import app.sotchi.domain.generic.UserRole
 import org.jetbrains.exposed.v1.core.dao.id.IntIdTable
 import org.jetbrains.exposed.v1.datetime.timestamp
 
-const val MAX_VARCHAR_LENGTH = 50
 
-object UserTable : IntIdTable() {
-    val name = varchar("name", MAX_VARCHAR_LENGTH)
-    val email = varchar("email", MAX_VARCHAR_LENGTH).uniqueIndex()
+object UserTable : IntIdTable("user") {
+    val name = varchar("name", 50)
+    val email = varchar("email", 64).uniqueIndex()
     val password = varchar("password", 256)
     val createdAtDt = timestamp("created_at_dt")
     val lastModifiedDt = timestamp("last_modified_dt")
