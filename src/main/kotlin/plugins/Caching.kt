721e410d7983e0f0af000802a01473745c1388ec
Paul Mitsch <paul.herzog@urlaubsguru.de>
2026-04-08 22:53:16 +0200

    feat<#14>: Add global default config > we go NoCache per default and configure it more restrictive on a per route scope

--- src/main/kotlin/plugins/Caching.kt
+++ src/main/kotlin/plugins/Caching.kt
@@ -4,14 +4,17 @@ import io.ktor.http.*
 import io.ktor.http.content.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.cachingheaders.*
+import io.ktor.server.sessions.Cache
 
+/**
+ * As mentioned in this article -> https://www.debugbear.com/docs/http-cache-control-header
+ * We implemented it to be NoCache per default
+ * And for user-sensitive data or user-specific data we restrict it further by going NoStore on route scope
+ */
 fun Application.configureCaching() {
     install(CachingHeaders) {
         options { call, outgoingContent ->
-            when (outgoingContent.contentType?.withoutParameters()) {
-                ContentType.Text.CSS -> CachingOptions(CacheControl.MaxAge(maxAgeSeconds = 24 * 60 * 60))
-                else -> null
-            }
+            CachingOptions(CacheControl.NoCache(visibility = CacheControl.Visibility.Private))
         }
     }
 }
