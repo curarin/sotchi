bb9538de43436b522017694522c89ab169ec4c1f
Paul Mitsch <paul.herzog@urlaubsguru.de>
2026-04-12 07:54:45 +0200

    feat<#23>: Add initial CORS configuration - ToDo Host change for production
    API and frontend are most likely not running on same machine and port so we need CORS. The final configuration
    for allowed hosts, schemes and subdomains are done before we Go Live (ticket created here -> #26).

--- src/main/kotlin/plugins/CrossOriginResourceSharing.kt
+++ src/main/kotlin/plugins/CrossOriginResourceSharing.kt
@@ -1,2 +1,18 @@
 package app.sotchi.plugins
 
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.cors.routing.*
+
+fun Application.configureCors() {
+    install(CORS) {
+        allowCredentials = true
+        allowMethod(HttpMethod.Post)
+        allowMethod(HttpMethod.Get)
+        allowMethod(HttpMethod.Delete)
+        allowMethod(HttpMethod.Patch)
+        allowHeader(HttpHeaders.Authorization)
+        anyHost() // @TODO: https://ktor.io/docs/server-cors.html#hosts -> Für Produktiv anpassen an Host
+    }
+
+}
