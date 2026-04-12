ab630972ec8e7cfde149c83bfaebd11977eb1e9b
Paul Mitsch <paul.herzog@urlaubsguru.de>
2026-04-12 07:37:41 +0200

    chore<#15>: Add hsts configuration to application

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -8,6 +8,7 @@ fun main(args: Array<String>) {
 }
 
 fun Application.module() {
+    configureStrictTransportSecurity()
     configureDefaultHeader()
     configureRatelimit()
     configureCaching()
