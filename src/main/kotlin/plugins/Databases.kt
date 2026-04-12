93a13aef3549f5956c6fc5db99ed797e4ce3c724
Paul Mitsch <paul.herzog@urlaubsguru.de>
2026-04-12 18:52:14 +0200

    feat<#20>: Refactor db connection injection to be based on .conf file
    We want to mock for integration tests and therefore need to inject a h2 in-mem instance during integration tests, this can be done when loading db data from a .conf file because we simply override it during integration test config

--- src/main/kotlin/plugins/Databases.kt
+++ src/main/kotlin/plugins/Databases.kt
@@ -10,16 +10,23 @@ import org.jetbrains.exposed.v1.jdbc.SchemaUtils
 import org.jetbrains.exposed.v1.jdbc.transactions.transaction
 
 fun Application.configureDatabases() {
+    val config = environment.config
+
+    val url = config.property("ktor.database.url").getString()
+    val user = config.property("ktor.database.user").getString()
+    val password = config.property("ktor.database.password").getString()
+    val driver = config.property("ktor.database.driver").getString()
+
     val database = Database.connect(
-        url = "jdbc:h2:./dev",
-        user = "root",
-        driver = "org.h2.Driver",
-        password = "root",
+        url = url,
+        user = user,
+        driver = driver,
+        password = password,
         databaseConfig = DatabaseConfig {
             defaultMaxAttempts = 3
         }
     )
-    transaction {
+    transaction(database) {
         SchemaUtils.create(UserTable, UserRoleTable, UserActivationTable)
     }
 }
