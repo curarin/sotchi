0f2cdb65a3b3822dd645ce6c23b298e3e584e6d2
Paul Mitsch <paul.herzog@urlaubsguru.de>
2026-04-12 07:22:19 +0200

    feat<#16>: Add DefaultHeader Custom Implementation
    Implements the following:
     - Override server with 'API' to prevent leaking unwanted information about server (was ktor/3.4.2 before)
     - Overriding Content Type so we can also provide the charset with utf-8
     - Add ContentDisposition as inline so clients dont download or smth
     - Add CSP Header to prevent XSS attacks
     - Add ContentTypeOptions to nosniff to prevent mime-type sniffing

--- src/main/kotlin/plugins/DefaultHeader.kt
+++ src/main/kotlin/plugins/DefaultHeader.kt
@@ -1,2 +1,17 @@
 package app.sotchi.plugins
 
+import io.ktor.http.HttpHeaders
+import io.ktor.server.application.*
+import io.ktor.server.plugins.defaultheaders.*
+
+fun Application.configureDefaultHeader() {
+    install(DefaultHeaders) {
+        header(HttpHeaders.Server, "API")
+        header(HttpHeaders.ContentType, "application/json; charset=utf-8")
+        header(HttpHeaders.ContentDisposition, "inline; charset=utf-8")
+        // Verhindert XSS-attacken > https://dev.to/sudiip__17/-important-http-response-headers-every-developer-should-know-4o2a
+        header("Content-Security-Policy", "default-src 'self'")
+        // Verhindert MIME-type sniffing attacken
+        header("X-Content-Type-Options", "nosniff")
+    }
+}
