a580b103ffde192ce02ce4dc45bb0fe33ae31013
Paul Mitsch <paul.herzog@urlaubsguru.de>
2026-07-04 23:03:29 +0200

    fix<#28>: Revert CPS as of now, because swagger doesnt work with CPS as restrictive as it is now
    For future reference: We need to reroute swagger to not listen to CSP if we wanna fix that - because CSP is important in production later on

--- src/main/kotlin/plugins/DefaultHeader.kt
+++ src/main/kotlin/plugins/DefaultHeader.kt
@@ -7,10 +7,10 @@ import io.ktor.server.plugins.defaultheaders.*
 fun Application.configureDefaultHeader() {
     install(DefaultHeaders) {
         header(HttpHeaders.Server, "API")
-        header(HttpHeaders.ContentType, "application/json; charset=utf-8")
         header(HttpHeaders.ContentDisposition, "inline; charset=utf-8")
         // Verhindert XSS-attacken > https://dev.to/sudiip__17/-important-http-response-headers-every-developer-should-know-4o2a
-        header("Content-Security-Policy", "default-src 'self'")
+        // Aktuell auskommentiert aufgrund von Swagger
+        //header("Content-Security-Policy", "default-src 'self'")
         // Verhindert MIME-type sniffing attacken
         header("X-Content-Type-Options", "nosniff")
     }
